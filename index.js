450e0516e191842d4ce27a846f42b285df332ae8
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-12-10 21:45:34 +0530

    fractal ring

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ addEventListener("load", () => {
 
   // canvas settings
   ctx.fillStyle = "green";
-  ctx.strokeStyle = "yellow";
   ctx.lineCap = "round";
   ctx.lineWidth = 30;
 
+  ctx.shadowColor = "rgb(0,0,0,0.7)";
+  ctx.shadowOffsetX = 5;
+  ctx.shadowOffsetY = 5;
+  ctx.shadowBlur = 10;
+
   // effect settings
   const size = 200;
-  const sides = 4;
+  const sides = 5;
   const maxLevel = 4;
   const scale = 0.5;
-  const spread = 1.57;
+  const spread = 0.7;
   const branches = 2;
+  let color = 120;
 
   ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.rotate(-0.314);
 
   function drawBranch(level) {
     if (level > maxLevel) return;
@@ -45,11 +51,30 @@ addEventListener("load", () => {
   }
 
   function drawFractal() {
+    ctx.strokeStyle = "hsl(" + color + ", 100%, 50%)";
     for (let i = 0; i < sides; i++) {
       ctx.rotate((Math.PI * 2) / sides);
       drawBranch(0);
     }
     ctx.restore();
   }
-  drawFractal();
+
+  // fps settings
+  let fps = 144;
+  let lastPrint = 0;
+  let timer = 0;
+  let delta = 1000 / fps;
+
+  function animate(timestamp) {
+    // const duration = timestamp - lastPrint;
+    // if (duration >= delta) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawFractal();
+    color = (color + 1) % 360;
+    lastPrint = timestamp;
+    // }
+    // timer++;
+    requestAnimationFrame(animate);
+  }
+  animate(timer);
 });
