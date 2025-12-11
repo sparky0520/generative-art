713d6303511d123a07329654e51b414a28da669d
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-12-11 17:22:01 +0530

    randomize fractals

--- index.js
+++ index.js
@@ -16,15 +16,17 @@ ctx.shadowBlur = 10;
 // effect settings
 const size =
   canvas.width < canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
-const sides = 5;
+
 const maxLevel = 4;
-const scale = 0.5;
-const spread = 0.7;
 const branches = 2;
+
+let sides = 5;
+let scale = 0.5;
+let spread = 0.7;
 let color = 120;
 
-ctx.translate(canvas.width / 2, canvas.height / 2);
-ctx.rotate(-0.314);
+// controls
+const randomizeButton = document.getElementById("randomizeButton");
 
 function drawBranch(level) {
   if (level > maxLevel) return;
@@ -36,44 +38,43 @@ function drawBranch(level) {
   for (let i = 0; i < branches; i++) {
     ctx.save();
     ctx.translate(size - (size / branches) * i, 0);
-    ctx.rotate(spread);
     ctx.scale(scale, scale);
+
+    ctx.save();
+    ctx.rotate(spread);
     drawBranch(level + 1);
     ctx.restore();
 
     ctx.save();
-    ctx.translate(size - (size / branches) * i, 0);
-    ctx.scale(scale, scale);
     ctx.rotate(-spread);
     drawBranch(level + 1);
+    ctx.restore();
+
     ctx.restore();
   }
 }
 
 function drawFractal() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
   ctx.strokeStyle = "hsl(" + color + ", 100%, 50%)";
+  ctx.translate(canvas.width / 2, canvas.height / 2);
   for (let i = 0; i < sides; i++) {
     ctx.rotate((Math.PI * 2) / sides);
     drawBranch(0);
   }
   ctx.restore();
 }
+drawFractal();
 
-// fps settings
-let fps = 144;
-let lastPrint = 0;
-let timer = 0;
-let delta = 1000 / fps;
-
-function animate(timestamp) {
-  // const duration = timestamp - lastPrint;
-  // if (duration >= delta) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawFractal();
-  color = (color + 1) % 360;
-  lastPrint = timestamp;
-  // }
-  // timer++;
-  requestAnimationFrame(animate);
+function randomizeFractal() {
+  sides = Math.floor(Math.random() * 7 + 2);
+  scale = Math.random() * 0.2 + 0.4;
+  spread = Math.random() * 2.9 + 0.1;
+  color = Math.random() * 360;
 }
-animate(timer);
+
+randomizeButton.addEventListener("click", () => {
+  randomizeFractal();
+  drawFractal();
+});
