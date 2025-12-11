288ac0281d7331f8a069ada4f30b152bd5e22ce4
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-12-11 16:14:24 +0530

    responsive fractal size

--- index.js
+++ index.js
@@ -1,80 +1,79 @@
-addEventListener("load", () => {
-  const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-  // canvas settings
-  ctx.fillStyle = "green";
-  ctx.lineCap = "round";
-  ctx.lineWidth = 30;
+// canvas settings
+ctx.fillStyle = "green";
+ctx.lineCap = "round";
+ctx.lineWidth = 30;
 
-  ctx.shadowColor = "rgb(0,0,0,0.7)";
-  ctx.shadowOffsetX = 5;
-  ctx.shadowOffsetY = 5;
-  ctx.shadowBlur = 10;
+ctx.shadowColor = "rgb(0,0,0,0.7)";
+ctx.shadowOffsetX = 5;
+ctx.shadowOffsetY = 5;
+ctx.shadowBlur = 10;
 
-  // effect settings
-  const size = 200;
-  const sides = 5;
-  const maxLevel = 4;
-  const scale = 0.5;
-  const spread = 0.7;
-  const branches = 2;
-  let color = 120;
+// effect settings
+const size =
+  canvas.width < canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
+const sides = 5;
+const maxLevel = 4;
+const scale = 0.5;
+const spread = 0.7;
+const branches = 2;
+let color = 120;
 
-  ctx.translate(canvas.width / 2, canvas.height / 2);
-  ctx.rotate(-0.314);
+ctx.translate(canvas.width / 2, canvas.height / 2);
+ctx.rotate(-0.314);
 
-  function drawBranch(level) {
-    if (level > maxLevel) return;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(size, 0);
-    ctx.stroke();
+function drawBranch(level) {
+  if (level > maxLevel) return;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(size, 0);
+  ctx.stroke();
 
-    for (let i = 0; i < branches; i++) {
-      ctx.save();
-      ctx.translate(size - (size / branches) * i, 0);
-      ctx.rotate(spread);
-      ctx.scale(scale, scale);
-      drawBranch(level + 1);
-      ctx.restore();
+  for (let i = 0; i < branches; i++) {
+    ctx.save();
+    ctx.translate(size - (size / branches) * i, 0);
+    ctx.rotate(spread);
+    ctx.scale(scale, scale);
+    drawBranch(level + 1);
+    ctx.restore();
 
-      ctx.save();
-      ctx.translate(size - (size / branches) * i, 0);
-      ctx.scale(scale, scale);
-      ctx.rotate(-spread);
-      drawBranch(level + 1);
-      ctx.restore();
-    }
-  }
-
-  function drawFractal() {
-    ctx.strokeStyle = "hsl(" + color + ", 100%, 50%)";
-    for (let i = 0; i < sides; i++) {
-      ctx.rotate((Math.PI * 2) / sides);
-      drawBranch(0);
-    }
+    ctx.save();
+    ctx.translate(size - (size / branches) * i, 0);
+    ctx.scale(scale, scale);
+    ctx.rotate(-spread);
+    drawBranch(level + 1);
     ctx.restore();
   }
+}
 
-  // fps settings
-  let fps = 144;
-  let lastPrint = 0;
-  let timer = 0;
-  let delta = 1000 / fps;
-
-  function animate(timestamp) {
-    // const duration = timestamp - lastPrint;
-    // if (duration >= delta) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawFractal();
-    color = (color + 1) % 360;
-    lastPrint = timestamp;
-    // }
-    // timer++;
-    requestAnimationFrame(animate);
+function drawFractal() {
+  ctx.strokeStyle = "hsl(" + color + ", 100%, 50%)";
+  for (let i = 0; i < sides; i++) {
+    ctx.rotate((Math.PI * 2) / sides);
+    drawBranch(0);
   }
-  animate(timer);
-});
+  ctx.restore();
+}
+
+// fps settings
+let fps = 144;
+let lastPrint = 0;
+let timer = 0;
+let delta = 1000 / fps;
+
+function animate(timestamp) {
+  // const duration = timestamp - lastPrint;
+  // if (duration >= delta) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFractal();
+  color = (color + 1) % 360;
+  lastPrint = timestamp;
+  // }
+  // timer++;
+  requestAnimationFrame(animate);
+}
+animate(timer);
